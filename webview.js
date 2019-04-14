2136ef1bb22dddb6f7d0917e7a3ac0599ac2b769
David Bensoussan <minipada@gmail.com>
2019-04-14 18:22:03 +0200

    feat: add counter with todays's events and tasks

--- webview.js
+++ webview.js
@@ -1,1 +1,12 @@
-"use strict";
+module.exports = Franz => {
+
+  getMessages = () => {
+    const events = document.querySelectorAll(".today")[0].getElementsByClassName('list')[0].getElementsByClassName('task-list')[0].getElementsByClassName('event-section')[0].getElementsByClassName('droppable')[0].children[0].childElementCount
+    const tasks = document.querySelectorAll(".today")[0].getElementsByClassName('list')[0].getElementsByClassName('task-list')[0].getElementsByClassName('task-section')[0].getElementsByClassName('droppable')[0].children[0].childElementCount
+
+    Franz.setBadge(events + tasks);
+  };
+
+  Franz.loop(getMessages);
+
+}
